e17eb480e82bacb9341f575dd2bede34612409c2
dave <ds.kim@dave-mac.local>
2021-10-07 16:26:10 +0900

    batch assignemnts

--- src/main/kotlin/se/ohou/example/PersonJob.kt
+++ src/main/kotlin/se/ohou/example/PersonJob.kt
@@ -9,17 +9,19 @@ import org.springframework.batch.item.ItemReader
 import org.springframework.batch.item.ItemWriter
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder
 import org.springframework.batch.item.json.JacksonJsonObjectMarshaller
-import org.springframework.batch.item.json.JsonFileItemWriter
 import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.FileSystemResource
 
 @Configuration
 class PersonJob(
     val jobBuilderFactory: JobBuilderFactory,
     val stepBuilderFactory: StepBuilderFactory
 ) {
+
+    private val RESOURCE_PATH = "${System.getProperty("user.dir")}/src/main/resources"
+
     @Bean
     fun personProcessingJob(): Job = jobBuilderFactory.get("personProcessingJob")
         .flow(personProcessingStep())
@@ -31,25 +33,38 @@ class PersonJob(
         // reader, processor, writer를 가진 tasklet step을 생성
         return stepBuilderFactory.get("personProcessingStep")
             .chunk<Person, Person>(1)
+            .reader(reader())
+            .processor(processor())
+            .writer(writer())
             .build()
     }
 
     @Bean
-    fun reader(): ItemReader<Person>? {
+    fun reader(): ItemReader<Person> {
         // sample-data.csv 파일을 읽어서 Person 도메인 객체로 컨버팅
-        return null
+        return FlatFileItemReaderBuilder<Person>()
+            .name("csvReader")
+            .resource(FileSystemResource("${RESOURCE_PATH}/sample-data.csv"))
+            .lineMapper { line, _ ->
+                val splitLine = line.split(",")
+                Person(splitLine[0], splitLine[1].toInt())
+            }.build()
     }
 
     @Bean
-    fun processor(): ItemProcessor<Person, Person>? {
-        // Person 객체안에 name을 모두 소문자로 변환
-        return null
+    fun processor(): ItemProcessor<Person, Person> {
+        return ItemProcessor {
+            Person(it.name.lowercase(), it.age)
+        }
     }
 
-
     @Bean
-    fun writer(): ItemWriter<Person>? {
+    fun writer(): ItemWriter<Person> {
         // Person 객체를 json 파일로 write (파일 위치는 상관없음)
-        return null
+        return JsonFileItemWriterBuilder<Person>()
+            .name("jsonWriter")
+            .jsonObjectMarshaller(JacksonJsonObjectMarshaller<Person>())
+            .resource(FileSystemResource("${RESOURCE_PATH}/sample-data.json"))
+            .build()
     }
 }
